c286a5fc1d245c6cbeb0922c5d0e7d03ccb98f58
Dezyop2 <wlebouvier@normandiewebschool.fr>
2024-02-14 19:39:19 +0100

    v1
    debut du giga projet

---
+++ js/download.js
@@ -0,0 +1,13 @@
+var downloadButton = document.getElementById("downloadButton");
+
+downloadButton.addEventListener("click", function() {
+    var link = document.createElement("a");
+    link.download = "cv.pdf"; 
+    link.href = "img//cv.pdf";
+    link.click();
+    downloadButton.classList.add("clicked");
+});
+
+downloadButton.addEventListener("mouseup", function() {
+    downloadButton.classList.remove("clicked");
+});
